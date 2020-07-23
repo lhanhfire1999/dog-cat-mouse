7b52fc007f4e8f997d5b7853629d39677c742eda
AnhLe <lhanhfire1999@gmail.com>
2020-07-23 13:31:34 +0700

    Cat eat Dog method

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ var dog = new Dog("Husky");
 var cat = new Cat('Tom');
 var mouse = new Mouse('Jerry')
 
-mouse.die();
-cat.eat(mouse);
+// mouse.die();
+// cat.eat(mouse);
+try{
+cat.eat(dog);	
+}catch(error){
+	console.log('Error when Cat eating Dog');
+
+}
+
 console.log(cat);
