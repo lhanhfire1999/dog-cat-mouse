ac6f035fd740d9c7c7f761e90e93b5a7dd67a4c2
AnhLe <lhanhfire1999@gmail.com>
2020-07-23 23:02:52 +0700

    add sleep methor for horse

--- Horse.js
+++ Horse.js
@@ -5,7 +5,13 @@ Horse.prototype.run = function(){
 	console.log('Running');
 }
 
+
 Horse.prototype.jump=function(){
 	console.log('Jumped..........')
 }
+
+Horse.prototype.sleep = function(){
+	console.log('Sleeping..........');
+}
+
 module.exports=Horse;
